2628b564921f1a8bf3efeb2731f33bb958fdcd29
EC2 Default User <ec2-user@ip-172-31-18-168.us-east-2.compute.internal>
2018-03-26 23:32:58 +0000

    added banner

--- index.php
+++ index.php
@@ -143,7 +143,24 @@ if (empty($_GET['refresh'])) {
 			Powered by <a href="http://www.php.net/">PHP5</a> and <a href="mailto:<?php echo $author_email;?>?subject=<?php echo $author_project;?>"><?php echo $author_name.' Development'; ?></a>
 		</div>
 	</div> <!-- End Footer -->
-
+<div class="alignleft">
+     <script type="text/javascript">
+       	amzn_assoc_ad_type = "banner";
+	amzn_assoc_marketplace = "amazon";
+	amzn_assoc_region = "US";
+	amzn_assoc_placement = "assoc_banner_placement_default";
+	amzn_assoc_campaigns = "amazonvideoondemand";
+	amzn_assoc_banner_type = "category";
+	amzn_assoc_p = "12";
+	amzn_assoc_isresponsive = "false";
+	amzn_assoc_banner_id = "15GA2X24AC2V29CHDKG2";
+	amzn_assoc_width = "300";
+	amzn_assoc_height = "250";
+	amzn_assoc_tracking_id = "threadzbotsho-20";
+	amzn_assoc_linkid = "99c0d54eb6f4c440c4b997ddc265ee95";
+     </script>
+     <script src="//z-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1"></script>
+    </div>
 </div> <!-- End Page Container -->
 </body>
 </html>
